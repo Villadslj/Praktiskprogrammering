038216f938e5645c129bc2a132e3e3432b85c598
Villadslj <villadslj@aol.com>
2020-03-30 12:37:34 +0200

    second thing

---
+++ linking/world.cs
@@ -0,0 +1,7 @@
+
+using static System.Console;
+public class world{
+	public static void print(){
+	Write("hello form wolrd\n");
+}
+}
---
+++ math/main.cs
@@ -0,0 +1,17 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+class main{
+	static int Main(){
+	double x=1;
+	char c='ø';
+	string s="hello";
+	Write("sin({0})={1}\n",x,Sin(x));
+	Write($"sin({x})={Sin(x)}\n");
+	int i=100;
+	Write($"i={i}\n");
+	
+	return 0;
+}
+}
